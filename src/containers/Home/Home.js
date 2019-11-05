0968b3793cc93e542093994f5a791ff79ad3047b
Kelly Hougland <kelly.hougland@graybar.com>
2019-11-05 11:07:58 -0600

    moved initiative into it's own container

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -37,39 +37,6 @@ class Home extends Component {
   };
 
   render() {
-    let charView = this.props.chars
-      .sort((a, b) => (a.initiative < b.initiative ? 1 : -1))
-      .map(character => (
-        <p>
-          Name: {character.name} | Speed:{" "}
-          {character.speed + character.speedBonus}
-          <button onClick={() => this.props.speedIncrement(character.id)}>
-            +1
-          </button>
-          <button onClick={() => this.props.speedDecrement(character.id)}>
-            -1
-          </button>{" "}
-          | Initiative: {character.initiative}{" "}
-          <button onClick={() => this.props.initIncrement1(character.id)}>
-            +1
-          </button>{" "}
-          <button onClick={() => this.props.initDecrement1(character.id)}>
-            -1
-          </button>{" "}
-          <button onClick={() => this.props.initIncrement5(character.id)}>
-            +5
-          </button>{" "}
-          <button onClick={() => this.props.initDecrement5(character.id)}>
-            -5
-          </button>
-          | Turns Taken: {character.turnCount}
-          <button onClick={() => this.props.delChar(character.id)}>
-            Delete
-          </button>
-          <br />
-        </p>
-      ));
-
     let addCharForm = (
       <div>
         <input
@@ -95,8 +62,8 @@ class Home extends Component {
 
     return (
       <div>
-        {charView}
-        <button onClick={this.props.initiativePass}>Next Turn</button>
+        {/* {charView} */}
+        <Initiative />
         {addCharForm}
       </div>
     );
@@ -112,16 +79,7 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    initiativePass: () => dispatch(actionCreators.initiativePass()),
-    speedIncrement: id => dispatch(actionCreators.speedIncrement(id)),
-    speedDecrement: id => dispatch(actionCreators.speedDecrement(id)),
-    initIncrement1: id => dispatch(actionCreators.initIncrement1(id)),
-    initDecrement1: id => dispatch(actionCreators.initDecrement1(id)),
-    initIncrement5: id => dispatch(actionCreators.initIncrement5(id)),
-    initDecrement5: id => dispatch(actionCreators.initDecrement5(id)),
-    initChange: (value, id) => dispatch(actionCreators.initChange(value, id)),
-    addChar: character => dispatch(actionCreators.addChar(character)),
-    delChar: id => dispatch(actionCreators.delChar(id))
+    addChar: character => dispatch(actionCreators.addChar(character))
   };
 };
 
--- src/containers/Initiative/Initiative.js
+++ src/containers/Initiative/Initiative.js
@@ -1,6 +1,73 @@
 import React, { Component } from "react";
 import { Table } from "semantic-ui-react";
+import { connect } from "react-redux";
 
-class Initiative extends Component {}
+import * as actionCreators from "../../store/actions/index";
 
-export default Initiative;
+class Initiative extends Component {
+  render() {
+    let charView = this.props.chars
+      .sort((a, b) => (a.initiative < b.initiative ? 1 : -1))
+      .map(character => (
+        <p>
+          Name: {character.name} | Speed:{" "}
+          {character.speed + character.speedBonus}
+          <button onClick={() => this.props.speedIncrement(character.id)}>
+            +1
+          </button>
+          <button onClick={() => this.props.speedDecrement(character.id)}>
+            -1
+          </button>{" "}
+          | Initiative: {character.initiative}{" "}
+          <button onClick={() => this.props.initIncrement1(character.id)}>
+            +1
+          </button>{" "}
+          <button onClick={() => this.props.initDecrement1(character.id)}>
+            -1
+          </button>{" "}
+          <button onClick={() => this.props.initIncrement5(character.id)}>
+            +5
+          </button>{" "}
+          <button onClick={() => this.props.initDecrement5(character.id)}>
+            -5
+          </button>
+          | Turns Taken: {character.turnCount}
+          <button onClick={() => this.props.delChar(character.id)}>
+            Delete
+          </button>
+          <br />
+        </p>
+      ));
+    return (
+      <div>
+        {charView}{" "}
+        <button onClick={this.props.initiativePass}>Next Turn</button>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    chars: state.characters
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    initiativePass: () => dispatch(actionCreators.initiativePass()),
+    speedIncrement: id => dispatch(actionCreators.speedIncrement(id)),
+    speedDecrement: id => dispatch(actionCreators.speedDecrement(id)),
+    initIncrement1: id => dispatch(actionCreators.initIncrement1(id)),
+    initDecrement1: id => dispatch(actionCreators.initDecrement1(id)),
+    initIncrement5: id => dispatch(actionCreators.initIncrement5(id)),
+    initDecrement5: id => dispatch(actionCreators.initDecrement5(id)),
+    initChange: (value, id) => dispatch(actionCreators.initChange(value, id)),
+    delChar: id => dispatch(actionCreators.delChar(id))
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Initiative);
