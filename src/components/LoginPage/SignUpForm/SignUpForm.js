1d971d9b0844346ac4b0629a4a980caf9cf750b4
Kelly Hougland <kelly.hougland@graybar.com>
2019-11-11 08:22:06 -0600

    forgot a line on signup form

--- src/components/LoginPage/SignUpForm/SignUpForm.js
+++ src/components/LoginPage/SignUpForm/SignUpForm.js
@@ -6,35 +6,71 @@ const signUpForm = props => {
     <Form>
       <Form.Field>
         <label>Email:</label>
-        <Input type="email" placeholder="Email" onChange={(event) => props.propsToPass.changeEmail(event.target.value)} />
+        <Input
+          type="email"
+          placeholder="Email"
+          onChange={event => props.propsToPass.changeEmail(event.target.value)}
+        />
         <p>Please enter a valid email.</p>
       </Form.Field>
       <Form.Field>
         <label>Confirm Email:</label>
-        <Input type="email" placeholder="Confirm Email"  onChange={(event) => props.propsToPass.changeEmailConfirm(event.target.value)}/>
+        <Input
+          type="email"
+          placeholder="Confirm Email"
+          onChange={event =>
+            props.propsToPass.changeEmailConfirm(event.target.value)
+          }
+        />
         <p>Emails must match.</p>
       </Form.Field>
       <Form.Field>
         <label>Username:</label>
-        <Input type="text" placeholder="Last Name"  onChange={(event) => props.propsToPass.changeUserName(event.target.value)}/>
+        <Input
+          type="text"
+          placeholder="Last Name"
+          onChange={event =>
+            props.propsToPass.changeUserName(event.target.value)
+          }
+        />
         <p>Username must be at least 6 but no more than 14 characters long.</p>
       </Form.Field>
       <Form.Field>
         <label>Password:</label>
-        <Input type="password" placeholder="Password"  onChange={(event) => props.propsToPass.changePassword(event.target.value)}/>
+        <Input
+          type="password"
+          placeholder="Password"
+          onChange={event =>
+            props.propsToPass.changePassword(event.target.value)
+          }
+        />
         <p>Password must be at least 9 characters long.</p>
       </Form.Field>
       <Form.Field>
         <label>Confirm Password:</label>
-        <Input type="password" placeholder="Confirm Password"  onChange={(event) => props.propsToPass.changePasswordConfirm(event.target.value)}/>
-        <p></p>
+        <Input
+          type="password"
+          placeholder="Confirm Password"
+          onChange={event =>
+            props.propsToPass.changePasswordConfirm(event.target.value)
+          }
+        />
+        <p>Passwords must match.</p>
       </Form.Field>
       <Form.Field>
-        <label >I have read and accept the terms and conditions:</label>
+        <label>I have read and accept the terms and conditions:</label>
         <Checkbox onChange={props.propsToPass.acceptTerms} />
-        <p onClick={props.terms}>Click here to read the terms and conditions.</p>
+        <p onClick={props.terms}>
+          Click here to read the terms and conditions.
+        </p>
       </Form.Field>
-      <Button type="submit" disabled={props.noSubmit} onClick={props.submitAction}>Submit</Button>
+      <Button
+        type="submit"
+        disabled={props.noSubmit}
+        onClick={props.submitAction}
+      >
+        Submit
+      </Button>
     </Form>
   );
 };
