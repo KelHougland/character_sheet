3878e3eaa3fac88ae429fefce69f80d64c424958
KelHougland <knhougland@gmail.com>
2019-11-20 21:37:18 -0600

    initializing auth

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,1 +1,36 @@
 import * as actionTypes from "./actionTypes";
+
+export const authStart = () => {
+  return { type: actionTypes.AUTH_START };
+};
+
+export const authSuccess = (token, userId) => {
+  return { type: actionTypes.AUTH_SUCCESS, idToken: token, userId: userId };
+};
+
+export const authFail = error => {
+  return { type: actionTypes.AUTH_FAIL, error: error };
+};
+
+export const checkAuthTimeout = expirationTime => {
+  return dispatch => {
+    setTimeout(() => {
+      dispatch(logout());
+    }, expirationTime * 1000);
+  };
+};
+
+export const logout = () => {
+    return {type: actionTypes.AUTH_LOGOUT}
+}
+
+export const auth = (email, password, view) => {
+    return dispatch => {
+        dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        }
+    }
+}
